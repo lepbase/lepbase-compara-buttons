3c04ea9362af0bd1b60d8f35815fa8c3ea0d4b56
Richard Challis <rjchallis@gmail.com>
2016-07-01 10:25:37 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lepbase-compara-buttons
+Adds buttons to Ensembl gene/transcript pages to make gene trees easier to find
---
+++ modules/EnsEMBL/Web/Component/Shared.pm
@@ -0,0 +1,44 @@
+=head1 LICENSE
+
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+=head1 MODIFICATIONS
+
+Copyright [2014-2016] University of Edinburgh
+
+All modifications licensed under the Apache License, Version 2.0, as above.
+
+=cut
+
+package EnsEMBL::Web::Component::Shared;
+
+use strict;
+
+##################################
+### BEGIN LEPBASE MODIFICATIONS...
+##################################
+sub gene_tree_button {
+    my $obj = shift;
+    my ($url,$label) = @_;
+    my $button = '<a href="'.$label.'">$url</a>';
+    return $button;
+}
+
+##################################
+### ...END LEPBASE MODIFICATIONS
+##################################
+1;
